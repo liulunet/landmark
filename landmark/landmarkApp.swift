d33ff72521782849e73e3dae6fa64e5b804dbbc2
liulu <liulunet@live.com>
2023-09-02 21:17:37 +0800

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/2.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
