798d273e847ce128c38b52076f3c565d11fb790e
liulu <liulunet@live.com>
2023-09-02 23:05:30 +0800

    List and Navigation

---
+++ landmark/View/LandmarkRow.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkRow.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/2.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
