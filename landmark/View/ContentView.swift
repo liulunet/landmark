008d185108198ea337d2a8b0ab68f8451162151c
liulu <liulunet@live.com>
2023-09-02 23:05:58 +0800

    List and Navigation

---
+++ landmark/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
