4d6993fb29d1eaadde3082b368bf6cfe01b7b638
liulu <liulunet@live.com>
2023-09-03 00:05:55 +0800

    Handing User Input

---
+++ landmark/View/Landmarks/LandmarkList.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkList.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/2.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+//        ForEach(["iPhone SE (3rd generation)","iPhone 14"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
