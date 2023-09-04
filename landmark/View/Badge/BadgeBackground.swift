506e9f9d7447860799f117b95d77824662d114d8
liulu <liulunet@live.com>
2023-09-04 18:09:08 +0800

    Badge

---
+++ landmark/View/Badge/BadgeBackground.swift
@@ -0,0 +1,57 @@
+//
+//  BadgeBackground.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/4.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let heigh = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: heigh * (0.2 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(to: CGPoint (
+                        x: width * segment.line.x + xOffset,
+                        y: heigh * segment.line.y))
+                    
+                    path.addQuadCurve(to:
+                                        CGPoint(
+                                            x: width * segment.curve.x + xOffset,
+                                            y: heigh * segment.curve.y),
+                                      control:
+                                        CGPoint(
+                                            x: width * segment.control.x + xOffset,
+                                            y: heigh * segment.control.y))
+                }
+            }
+            .fill(.linearGradient(
+                            Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                            startPoint: UnitPoint(x: 0.5, y: 0),
+                            endPoint: UnitPoint(x: 0.5, y: 0.6)
+                        ))
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
---
+++ landmark/View/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/4.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
