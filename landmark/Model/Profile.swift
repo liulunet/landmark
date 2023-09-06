3ecaba8dc7f8adcb47800fc09acbdc8fb2621f45
liulu <liulunet@live.com>
2023-09-06 19:30:52 +0800

    Working with UI Controls

---
+++ landmark/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  landmark
+//
+//  Created by liulu on 2023/9/6.
+//
+
+import Foundation
+
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌹"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "⛄️"
+        
+        var id: String { rawValue }
+    }
+}
